1469c1951469a388b2d5d97cb81aaff4fb29ba6b
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-06-28 20:30:52 +0900

    Update CHaser_test.py

--- CHaser_test.py
+++ CHaser_test.py
@@ -1,17 +1,19 @@
 import CHaser
-import random
-
+#0:なし（床）
+#1:相手
+#2:ブロック
+#3:アイテム
 client = CHaser.Client()
-
-def checkValue(value): #주위 9칸이 다이아가 있는가 확인
-    count = 0
-    for i in value:
-        if i == 3:
-            return 'ok'
-        else:
-            count += 1
-    return count
-def moveThink(): #상하좌우 9칸씩 확인
+#1.벽 확인
+#2.9칸 이내 다이아 확인
+def checkValue(value): #주위 9칸 탐색
+    if 3 in value:
+        return 'item'
+    elif 1 in value:
+        return 'enemy'
+    elif 2 in value:
+        return 'block'
+#def moveThink(): #상하좌우 9칸씩 확인
     valueL = client.look_left()
     valueR = client.look_right()
     valueU = client.look_up()
@@ -26,11 +28,11 @@ def moveThink(): #상하좌우 9칸씩 확인
         return 'D'
     else:
         return 'N'
-def moveAI():
+#def moveAI():
     value = moveThink()
     if value == 'L':
         value = client.look_left()
-def canmove(): #상하좌우 나가는 길 탐색
+#def canmove(): #상하좌우 나가는 길 탐색
     value = client.get_ready()
     value = [value[1], value[4], value[6], value[8]]
     if 0 not in value:
@@ -39,7 +41,18 @@ def canmove(): #상하좌우 나가는 길 탐색
         return 'print('')'
 def main():
     while(True):
-        eval(canmove())
+        value = client.walk_up()
+        value = client.walk_down()
+        value = client.walk_left()
+        value = client.walk_right()
+        RU = ['client.walk_right()','client.walk_up()']
+        UR = ['client.walk_up()','client.walk_right()']
+        RD = ['client.walk_right()','client.walk_down()']
+        DR = ['client.walk_down()','client.walk_right()']
+        LU = ['client.walk_left()','client.walk_up()']
+        UL = ['client.walk_up()','client.walk_left()']
+        LD = ['client.walk_left()','client.walk_down()']
+        DL = ['client.walk_down()','client.walk_left()']
         value = client.get_ready()
         if 1 in [value[1], value[4], value[6], value[8]]:
             value = [value[1], value[4], value[6], value[8]]
@@ -55,12 +68,11 @@ def main():
             else:
                 print('error')
                 exit()
-
-        if checkValue(value) != 'ok': #9칸 전부 다이아가 없을때
-            value = client.look_right()
-            if value[4] == 0:
-                moveThink()
         else:
-            pass #정상적인거
+            CV = checkValue(value)
+            if CV == 'item':
+                pass
+
+
 if __name__ == "__main__":
     main()
