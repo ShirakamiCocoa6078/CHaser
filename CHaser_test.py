ebeb74f8b9894d1dd2636785709fd0465b1546d3
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-07-12 19:33:21 +0900

    Update CHaser_test.py

--- CHaser_test.py
+++ CHaser_test.py
@@ -19,39 +19,6 @@ def checkValue(value): #주위 9칸 탐색
         return 'break'
     else:
         return 'print('')'
-def main():
-    while(True):
-        up = 'client.walk_up()'
-        down = 'client.walk_down()'
-        left = 'client.walk_left()'
-        right = 'client.walk_right()'
-        moveList = [[[up, left],[left,up]],[up],[[up, right],[right,up]],[left],[right],[[down, left],[left,down]],[down],[[down, right],[right,down]]]
-
-        value = client.get_ready() #ex : [2, 0, 0, 0, 0, 0, 0, 0, 2], chaserEx.png
-        print(value)
-        if 1 in [value[0], value[3], value[5], value[7]]: #상대 확인
-            value = [value[0], value[3], value[5], value[7]]
-            enemy = value.index(1)
-            if enemy == 0:
-                client.put_up()
-            elif enemy == 1:
-                client.put_left()
-            elif enemy == 2:
-                client.put_right()
-            elif enemy == 3:
-                client.put_down()
-            else:
-                print('error')
-                exit()
-        else:
-            CV = checkValue(value)
-            if CV == 'item':
-                mainItem = value.index(3)
-                if mainItem <= 3:
-                    []
-
-if __name__ == "__main__":
-    main()
 def moveChack(value, MainItem, way):
     MCList = [[0,1,2],[3,5],[7,8,9]]
     waypoint = {'up' : 0, 'middle' : 1, 'down' : 2}
@@ -84,7 +51,7 @@ def moveChack(value, MainItem, way):
             return 'left stop'
         else:
             return 'right stop'
-    else: #대충 손봄(자세히 다시보기)
+    else:
         if MainItem == 6:
             if (value[7] == 2 or value[3] == 2):
                 if (value[7] == 2 and value[3] == 2):
@@ -107,6 +74,44 @@ def moveChack(value, MainItem, way):
                     return 'down right'
             else:
                 return 'down right'
+def main():
+    while(True):
+        up = 'client.walk_up()'
+        down = 'client.walk_down()'
+        left = 'client.walk_left()'
+        right = 'client.walk_right()'
+        moveList = [[[up, left],[left,up]],[up],[[up, right],[right,up]],[left],[right],[[down, left],[left,down]],[down],[[down, right],[right,down]]]
+
+        value = client.get_ready() #ex : [2, 0, 0, 0, 0, 0, 0, 0, 2], chaserEx.png
+        if 1 in [value[0], value[3], value[5], value[7]]: #상대 확인
+            value = [value[0], value[3], value[5], value[7]]
+            enemy = value.index(1)
+            if enemy == 0:
+                client.put_up()
+            elif enemy == 1:
+                client.put_left()
+            elif enemy == 2:
+                client.put_right()
+            elif enemy == 3:
+                client.put_down()
+            else:
+                print('error')
+                #exit()
+        else:
+            CV = checkValue(value)
+            if CV == 'item':
+                mainItem = value.index(3)
+                if mainItem <= 3:
+                    moveChack(value,mainItem,'up')
+                elif 4<= mainItem <= 6:
+                    moveChack(value,mainItem,'middle')
+                elif mainItem >= 6:
+                    moveChack(value,mainItem,'down')
+                else:
+                    pass
+
+if __name__ == "__main__":
+    main()
 
 #def moveThink(): #상하좌우 9칸씩 확인
     #valueL = client.look_left()
