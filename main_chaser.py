4944de483fc4522b8d39976f47e8f0b999fd0843
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-07-19 20:29:39 +0900

    Update main_chaser.py

--- main_chaser.py
+++ main_chaser.py
@@ -1,12 +1,18 @@
 import CHaser
 import random
 wayList = ['up','left','down','right']
+wayListNum = {'up': '0 1 2', 'left': '0 3 6', 'down': '6 7 8','right': '2 5 8'}
 def main():
     value = []
     client = CHaser.Client()
     firstway = random.choice(wayList)
     while(True):
         value = client.get_ready()
+        for i in range(4):
+            for e in range(3):
+                WNum = [wayListNum[firstway].split(' ')]
+                if value[WNum[e]] in 0:
+                    firstway = random.choice(wayList)
 
 
 if __name__ == "__main__":
