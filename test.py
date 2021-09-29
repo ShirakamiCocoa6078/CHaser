54b442e1faaec03955f669d7dcf1fada4b6728b0
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-09-29 16:49:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,11 @@ def main():
     client = CHaser.Client()
     value = client.get_ready()
     print(value)
+    value = client.walk_right()
+    value = client.walk_right()
+    value = client.walk_right()
+    value = client.walk_right()
+    value = client.walk_right()
 
 if __name__ == "__main__":
     main()
